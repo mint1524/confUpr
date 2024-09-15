2d2a1cac8c552804cfd8e37098e3af5cebb811bb
mint1524 <94223742+mint1524@users.noreply.github.com>
2024-09-16 00:12:08 +0300

    Update kal5.sh

--- prak1/kal5.sh
+++ prak1/kal5.sh
@@ -1,4 +1,26 @@
-#1/bin/bash
+#!/bin/bash
 
-chmod +x $1
-sudo cp $1 /usr/local/bin
+command_name="$1"
+command_path="./$command_name"
+
+if [ ! -f "$command_path" ]; then
+	echo "File '$command_path' not found."
+	exit 1
+fi
+
+sudo chmod +x "$command_name"
+sudo cp "$command_path" /usr/local/bin/
+
+if [ $? -ne 0 ]; then
+	echo "Can't copy file."
+	exit 1
+fi
+
+sudo chmod 775 /usr/local/bin/"$command_name"
+
+if [ $? -ne 0 ]; then
+	echo "Can't set the rules for '$command_name'."
+	exit 1
+fi
+
+echo "File '$command_name' registrated successfully."
