3a3666143b842e4c012e200a01775962ce2954b2
mint1524 <94223742+mint1524@users.noreply.github.com>
2024-11-16 13:43:39 +0300

    Update test_emulator.py

--- hw1/test_emulator.py
+++ hw1/test_emulator.py
@@ -3,17 +3,32 @@ from emulator import ls_command, cd_command, head_command
 
 class TestShellCommands(unittest.TestCase):
 
-    def test_ls(self):
+    # Тесты для команды ls
+    def test_ls_root(self):
         files = ls_command('/', '/Users/min7t/emulator/virtual_fs.tar')
-        self.assertIn('home', files)
+        self.assertIn('home', files)  
 
-    def test_cd(self):
+    def test_ls_subdirectory(self):
+        files = ls_command('/home', '/Users/min7t/emulator/virtual_fs.tar')
+        self.assertIn('user', files) 
+
+    # Тесты для команды cd
+    def test_cd_to_home(self):
         new_dir = cd_command('/', 'home', '/Users/min7t/emulator/virtual_fs.tar')
-        self.assertEqual(new_dir, '/home')
+        self.assertEqual(new_dir, '/home')  
 
-    def test_head(self):
+    def test_cd_to_parent_directory(self):
+        new_dir = cd_command('/home/user', '..', '/Users/min7t/emulator/virtual_fs.tar')
+        self.assertEqual(new_dir, '/home') 
+
+    # Тесты для команды head
+    def test_head_valid_file(self):
         content = head_command('./home/user/file1.txt', '/Users/min7t/emulator/virtual_fs.tar')
-        self.assertTrue(len(content) > 0)
+        self.assertTrue(len(content) > 0) 
+
+    def test_head_nonexistent_file(self):
+        with self.assertRaises(FileNotFoundError): 
+            head_command('./home/user/nonexistent.txt', '/Users/min7t/emulator/virtual_fs.tar')
 
 if __name__ == '__main__':
     unittest.main()
